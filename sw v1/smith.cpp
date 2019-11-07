f0d647c3f5caa977cf0df74e5d92fa6e1b69d7c9
dysoxor <andreys833@gmail.com>
2019-11-07 16:31:00 +0100

    fasta1

--- sw v1/smith.cpp
+++ sw v1/smith.cpp
@@ -7,12 +7,16 @@
 
 using namespace std;
 
+const int gap = -2;
+const int match = +3;
+const int mismatch = -3;
+
 typedef struct{
 	int x;
 	int y;
 } Pos;
 
-string findPath(Pos pos, vector<vector<int>> tableau, string prot1, string prot2, int gap, int match, int mismatch){
+string findPath(Pos pos, vector<vector<int>> tableau, string prot1, string prot2){
 	Pos nextPos;
 	//printf("Je suis a la pos %i %i\n",pos.x, pos.y);
 	if (tableau[pos.x][pos.y] == 0 ){// || (pos.x == 0) || (pos.y == 0)){
@@ -81,17 +85,16 @@ int main(int argc, char** argv){
 	int len1 = prot1.size();
 	int len2 = prot2.size();
 	vector<vector<int>> tableau(len1+1, vector<int>(len2+1,0));
-	for (int i = 0; i <= len1 ; i++)
-		tableau[i][0]= 0;
-	for (int j = 0; j <= len2; j++)
-		tableau[0][j]=0;
+	//for (int i = 0; i <= len1 ; i++)
+		//tableau[i][0]= 0;
+	//for (int j = 0; j <= len2; j++)
+		//tableau[0][j]=0;
 
-	/*int gap = -2;
-	int match = +3;
-	int mismatch = -3;*/
-	int up = 0;
-	int left = 0;
-	int diag = 0;
+	int up, left, diag, max = 0, 0, 0, 0;
+	//int nb_of_max = 0; //pour un seul max pour l'instant
+	Pos pos;
+	pos.x=0;
+	pos.y=0;
 	for (int i = 1; i <= len1 ; i++){
 		for (int j = 1; j <= len2 ; j++){
 			up = tableau[i][j-1] + gap;
@@ -102,30 +105,16 @@ int main(int argc, char** argv){
 			else
 				diag+=mismatch;
 			tableau[i][j] = max(up,max(left,max(diag,0)));
+			if (tableau[i][j] > max){
+				max = tableau[i][j];
+				pos.x = i;
+				pos.y = j;
+			}
 		}
 	}
 
 	//Trouver les maxs (il faudra utiliser un meilleur algorithme pour le trouver)
 
-
-	int max = 0;
-	//int nb_of_max = 0; //pour un seul max pour l'instant
-	Pos pos;
-	pos.x=0;
-	pos.y=0;
-	for (int i = 0; i <= len1; i++){
-		for (int j = 0; j <= len2; j++){
-			if (tableau[i][j] > max){
-				max = tableau[i][j];
-				//nb_of_max = 1;
-				pos.x = i;
-				pos.y = j;
-			} /*else if (tableau[i][j] == max){
-				nb_of_max++;
-			}*/
-		}
-	}
-	//printf("Max en %i %i avec comme valeur %i\n",pos.x, pos.y, max);
 
 	string res = findPath(pos, tableau, prot1, prot2, gap, match, mismatch);
 	std::cout << "L'alignement de " << argv[1] << " et " << argv[2] << " donne " << res << " et a un score de "<< max << std::endl;
