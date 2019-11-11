3f8aef025a7bbe557e55c87d70b92dba9be6e33c
Kcnarf-exe <francktrouillez99@gmail.com>
2019-11-11 13:35:03 +0100

    maj

--- sw v1/smith.cpp
+++ sw v1/smith.cpp
@@ -9,20 +9,13 @@
 
 using namespace std;
 
-/*typedef struct{
-	int x;
-	int y;
-} Pos;*/
 
 string findPath(Position* pos, string prot1, string prot2, int gap, BlosumMatrix* blosum){
-	//Pos nextPos;
-	//printf("Je suis a la pos %i %i\n",pos.x, pos.y);
-	//if (tableau[pos.x][pos.y] == 0 ){// || (pos.x == 0) || (pos.y == 0)){
-		//printf("J'arrive à un 0\n");
+
 		cout << pos->getX() << " " << pos->getY() << endl;
 		if (pos->getValue() == 0){
 			return "";
-	} else {
+		} else {
 		string returnValue;
 		vector<Position*> rootPosVector = pos->getRoot();
 		Position* rootPos = rootPosVector[0];
@@ -49,55 +42,6 @@ string findPath(Position* pos, string prot1, string prot2, int gap, BlosumMatrix
 			returnValue+="-";
 		}
 		return returnValue;
-		//printf("Je suis pas encore à un 0\n");
-		/*int diag = tableau[pos.x-1][pos.y-1];
-		int up = tableau[pos.x][pos.y-1] + gap;
-		int left = tableau[pos.x-1][pos.y] + gap;*/
-
-		//int match=matrice->get(prot1.at(pos.x-1), prot2.at(pos.y-1));
-		/*if (prot1[pos.x-1] == prot2[pos.y-1])
-			diag += match;
-		else
-			diag+=mismatch;*/
-		/*diag+=match;
-		nextPos.x = pos.x - 1;
-		nextPos.y = pos.y - 1;
-		bool aa1 = true;
-		bool aa2 = true;
-		if (tableau[pos.x][pos.y] == left){
-			//printf("Je viens de la gauche\n");
-			nextPos.y ++;
-			aa2=false;
-		}
-		else if (tableau[pos.x][pos.y] == up){
-			//printf("Je viens d'en haut\n");
-			nextPos.x ++;
-			aa1=false;
-		}
-		else if(tableau[pos.x][pos.y] == diag){
-			//printf("Je viens de la diagonale\n");
-		}
-		else {
-			cout << "Problème dans le findpath" << endl;
-		}
-		string res = findPath(nextPos, tableau, prot1, prot2, gap, matrice);
-		string returnValue;
-		if (res != ""){
-			returnValue+=res;
-			returnValue+= "/";
-		}
-		if (aa1){
-			returnValue+=prot1[pos.x-1];
-		} else {
-			returnValue+="-";
-		}
-		if (aa2){
-			returnValue+=prot2[pos.y-1];
-		} else {
-			returnValue+="-";
-		}
-		//std::cout << returnValue << std::endl;
-		return returnValue;*/
 	}
 }
 
@@ -123,17 +67,13 @@ int main(int argc, char** argv){
 	int len1 = prot1.size();
 	int len2 = prot2.size();
 	Position* pos;
-
-	//vector<vector<int>> tableau(len1+1, vector<int>(len2+1,0));
 	for (int i = 0; i <= len1 ; i++){
 		pos=new Position(i,0);
 		matrice->addPosition(pos);
 	}
-		//tableau[i][0]= 0;
 	for (int j = 0; j <= len2; j++){
 		pos=new Position(0,j);
 		matrice->addPosition(pos);
-		//tableau[0][j]=0;
 	}
 
 
@@ -145,10 +85,7 @@ int main(int argc, char** argv){
 	int diag = 0;
 
 	//int nb_of_max = 0; //pour un seul max pour l'instant
-	//Pos pos;
 	Position* maxPos = new Position();
-	//pos.x=0;
-	//pos.y=0;
 	for (int i = 1; i <= len1 ; i++){
 		for (int j = 1; j <= len2 ; j++){
 			pos = new Position(i,j);
@@ -185,39 +122,12 @@ int main(int argc, char** argv){
 				cout<< maxPos->getValue()<< endl;
 
 			}
-
-			/*if (tableau[i][j] > vmax){
-				vmax = tableau[i][j];
-				//nb_of_max = 1;
-				pos.x = i;
-				pos.y = j;
-			} /*else if (tableau[i][j] == max){
-				nb_of_max++;
-			}*/
 		}
 	}
 
 	//Trouver les maxs (il faudra utiliser un meilleur algorithme pour le trouver)
 
 
-	/*int max = 0;
-	//int nb_of_max = 0; //pour un seul max pour l'instant
-	Pos pos;
-	pos.x=0;
-	pos.y=0;
-	for (int i = 0; i <= len1; i++){
-		for (int j = 0; j <= len2; j++){
-			if (tableau[i][j] > max){
-				max = tableau[i][j];
-				pos.x = i;
-				pos.y = j;
-			} *//*else if (tableau[i][j] == max){
-				nb_of_max++;
-			}*/
-		/*}
-	}*/
-	//printf("Max en %i %i avec comme valeur %i\n",pos.x, pos.y, max);
-	//cout << maxPos->getValue() <<" " << maxPos->getX() << " " << maxPos->getY() << endl;
 	matrice->print();
 	string res = findPath(maxPos, prot1, prot2, gap, blosum);
 	cout << "L'alignement de " << argv[1] << " et " << argv[2] << " donne " << res << " et a un score de "<< maxPos->getValue() << endl;
