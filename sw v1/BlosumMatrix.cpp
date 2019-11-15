e0414a422d6a51ab3d44833f71f1a76c2201507b
Kcnarf-exe <francktrouillez99@gmail.com>
2019-11-15 13:55:13 +0100

    Comments in the sw part

--- sw v1/BlosumMatrix.cpp
+++ sw v1/BlosumMatrix.cpp
@@ -1,59 +1,64 @@
 #include "BlosumMatrix.h"
 using namespace std;
 
+//Create BlosumMatrix
 BlosumMatrix::BlosumMatrix(string pathToBlosumMatrix){
-	cout<<"Matrice créée avec " << pathToBlosumMatrix<< endl;
+	cout<<"Matrix created with " << pathToBlosumMatrix<< endl;
 	BlosumMatrix::setup(pathToBlosumMatrix);
 };
 
+//Setup it
 void BlosumMatrix::setup(string pathToBlosumMatrix){
 	ifstream file(pathToBlosumMatrix);
 	string line;
 	bool first_line = true;
 	int value;
-	int n_ligne = 0;
-	int n_colonne = 0;
+	int n_line = 0;
+	int n_column = 0;
 	if (file.is_open()){
-		while(getline(file,line)){
-			if (line.at(0) != '#'){
-				n_colonne=0;
+		while(getline(file,line)){ //We check every line
+			if (line.at(0) != '#'){ //We don't do anything we the comments of the file
+				n_column=0;
 				if (first_line){
+					//If we are at the first line, we can read the character of the amino acid
 					first_line=false;
 					for (int i = 0; i < line.length();i++){
 						if (line.at(i) != ' '){
+							// If we get a character different than a void char, we put it in a map
 							charToInt.insert(pair<char,int>(line.at(i),n_colonne));
 							n_colonne++;
 						}
 					}
-					matrice.assign(charToInt.size(), vector<int> (charToInt.size(),0));
+					matrix.assign(charToInt.size(), vector<int> (charToInt.size(),0));
+					//We initialize the matrix with full 0
 				} else {
-					line.erase(0,1);//on enleve la lettre
+					line.erase(0,1);//remove the letter of the line
 					for (int i = 0; i< line.length();i++){
-						if (line.at(i) != ' ' && line.at(i) != '-'){
-							value = (int)line.at(i) -48; //ASCII digits commence à 48
+						if (line.at(i) != ' ' && line.at(i) != '-'){//We skip the '-' character
+							value = (int)line.at(i) -48; //ASCII digits starts at 48
 							if (i != 0 && line.at(i-1) == '-'){
+								//If there is a '-' before the int, we have a negative value
 								value = -value;
 							}
-							matrice[n_ligne][n_colonne] = value;
-							n_colonne++;
+							matrix[n_line][n_column] = value; //Set the value
+							n_column++;
 						}
 					}
-					n_ligne++;
+					n_line++;
 				}
 
 			}
 		}
 		file.close();
 	} else {
-		cout << "Problème dans l'ouverture du fichier blosum" << endl;
+		cout << "Problem while opening the blosum file" << endl;
 	}
 };
 
 const int BlosumMatrix::get(char aa1, char aa2){
 	int line = charToInt[aa1];
-	int colonne = charToInt[aa2];
-	//cout << line << " " <<colonne << " " << matrice[line][colonne]<< endl;
-	return matrice[line][colonne];
+	int column = charToInt[aa2];
+	return matrix[line][column];
 };
 
 const int BlosumMatrix::get(string aa1, string aa2){
@@ -61,9 +66,9 @@ const int BlosumMatrix::get(string aa1, string aa2){
 };
 
 const void BlosumMatrix::print(){
-	for (int i = 0; i < matrice.size(); i++){
-		for (int j = 0; j < matrice[0].size(); j++){
-			cout << matrice[i][j] << " ";
+	for (int i = 0; i < matrix.size(); i++){
+		for (int j = 0; j < matrix[0].size(); j++){
+			cout << matrix[i][j] << " ";
 		}
 		cout << endl;
 	}
