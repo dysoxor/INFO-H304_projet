966f62bb2be57aa525002181343c5c87c923c092
Kcnarf-exe <francktrouillez99@gmail.com>
2019-11-19 21:50:59 +0100

    opti sw

--- sw v1/smith.cpp
+++ sw v1/smith.cpp
@@ -51,7 +51,30 @@ using namespace std;
 	}
 }
 */
+int findMax(int tableau[], int size){
+	int res = tableau[0];
+	for (int i = 1; i < size; i++){
+		if (tableau[i]>res){
+			res = tableau[i];
+		}
+	}
+	return res;
+}
 
+int findMax2(int t1[], int t2[], int s1, int s2){
+	int res = t1[0];
+	for (int i = 1; i < s1; i++){
+		if (t1[i]> res){
+			res = t1[i];
+		}
+	}
+	for (int j = 0; j < s2; j++){
+		if(t2[j]>res){
+			res = t2[j];
+		}
+	}
+	return res;
+}
 
 int main(int argc, char** argv){
 	if (argc != 3){
@@ -60,7 +83,6 @@ int main(int argc, char** argv){
 		cout <<"Number of parameters given : " << (argc - 1) << endl;
 		return EXIT_FAILURE;
 	}
-	clock_t begin = clock();
 
 	List *listProtein1 = readFasta(argv[1]);
   string content1 = listProtein1->getHead()->getSequence();
@@ -72,7 +94,7 @@ int main(int argc, char** argv){
 	BlosumMatrix* blosum = new BlosumMatrix("blosum62");
 
 	//Initialization of the ScoringMatrix
-	ScoringMatrix* matrix = new ScoringMatrix();
+	//ScoringMatrix* matrix = new ScoringMatrix();
 
 	//define the 2 constants : gap opening and gap expansion
 	const int gap_op = 11;
@@ -93,7 +115,7 @@ int main(int argc, char** argv){
 
 	//First step of the algorithm is to place zeros
 	//in the first line and first column
-	for (int i = 0; i <= len1 ; i++){
+	/*for (int i = 0; i <= len1 ; i++){
 		pos=new Position(i,0);
 		matrix->addPosition(pos);
 	}
@@ -103,16 +125,72 @@ int main(int argc, char** argv){
 	}
 
 	matrix->setupMax(len1+1, len2+1);
-
+	*/
 	//Define variables used in order to fill the ScoringMatrix
 	int match;
 	int up = 0;
 	int left = 0;
 	int diag = 0;
 
-	int upgap;
-	int leftgap;
-	Position* maxPos = new Position();
+	/*int upgap;
+	int leftgap;*/
+
+
+	/*int maxX = 0;
+	int maxY = 0;*/
+	int maxValue = 0;
+	vector<vector<int>> matrice;//[len1+1][len2+1];
+	vector<int> tempv;
+	tempv.assign(len2+1,0);
+	matrice.assign(len1+1,tempv);
+	int maxLine[len2+1];
+	int maxColumn[len1+1];
+	int posXmaxLine[len1+1];
+	int posYmaxColumn[len2+1];
+	for (int i = 0; i < len1+1; i++){
+		posXmaxLine[i] = 0;
+		maxColumn[i] = 0;
+		matrice[i][0] = 0;
+	}
+	for (int j = 0; j < len2+1; j++){
+		posYmaxColumn[j] = 0;
+		maxLine[j] = 0;
+		matrice[0][j] = 0;
+	}
+	int temp[4];
+	clock_t begin = clock();
+	int tempVal;
+	int mline;
+	int mcolumn;
+	for (int i = 1; i < len1+1; i++){
+		for (int j = 1; j < len2+1; j++){
+			mcolumn = maxColumn[i];
+			mline = maxLine[j];
+			temp[0] = mcolumn - gap_op - gap_ex*(j - posYmaxColumn[i]);
+			temp[1] = mline - gap_op - gap_ex*(i - posXmaxLine[j]);
+			temp[2] = matrice[i-1][j-1] + blosum->get(prot1.at(i-1), prot2.at(j-1));
+			temp[3] = 0;
+			tempVal = findMax(temp,4);
+			matrice[i][j] = tempVal;
+			if (tempVal>mline){
+				maxLine[j] = tempVal;
+				posXmaxLine[j] = i;
+			}
+			if (tempVal > mcolumn){
+				maxColumn[i] = tempVal;
+				posYmaxColumn[i] = j;
+			}
+			/*if (tempVal > maxValue){
+				maxValue = matrice[i][j];
+				maxX = i;
+				maxY = j;
+			}*/
+		}
+	}
+	maxValue = findMax2(maxColumn, maxLine, len2+1, len1+1);
+	//int root[len1+1][len2+1];
+
+	/*Position* maxPos = new Position();
 	//Position* allPos = (Position*)malloc(sizeof(Position)*(len1+1)*(len2+1));
 
 	/*cout << (len1+1)*(len2+1) << endl;
@@ -123,14 +201,14 @@ int main(int argc, char** argv){
 	}
 	cout << "or here" << endl;*/
 	//We will go in every case to complete the matrix
-	for (int i = 1; i <= len1 ; i++){
+	/*for (int i = 1; i <= len1 ; i++){
 		for (int j = 1; j <= len2 ; j++){
-			pos = new Position(i,j);
+			//pos = new Position(i,j);
 			//cout << i << " " << j << " "<< (i-1)*len1+(j-1) << endl;
 			/*pos = &allPos[(i-1)*len1+(j-1)];
 			pos->setX(i);
 			pos->setY(j);*/
-			matrix->addPosition(pos);
+			//matrix->addPosition(pos);
 			/*upgap = gap_op;
 			leftgap = gap_op;
 			//We check if the root is from the upper case.
@@ -149,19 +227,19 @@ int main(int argc, char** argv){
 			*/
 
 			//We find the value in the BlosumMatrix
-			up = matrix->getMaxColumn(i)->getValue() - (gap_op + gap_ex*matrix->getDistYWithMax(pos));
+			/*up = matrix->getMaxColumn(i)->getValue() - (gap_op + gap_ex*matrix->getDistYWithMax(pos));
 			left = matrix->getMaxLine(j)->getValue() - (gap_op + gap_ex*matrix->getDistXWithMax(pos));
 			match=blosum->get(prot1.at(i-1), prot2.at(j-1));
 			diag = matrix->getValue(i-1,j-1);
 			diag+=match; // Add the result of the blosum matrix to the diagonal value
-
+*/
 			//Now, we find the maximum value. If they are all negative, we take 0.
-			int highValue = max(up,max(left,max(diag,0)));
+			/*int highValue = max(up,max(left,max(diag,0)));
 			//We set it to the actual position
 			pos->setValue(highValue);
 
 			matrix->setMaxLine(j, pos);
-			matrix->setMaxColumn(i, pos);
+			matrix->setMaxColumn(i, pos);*/
 			//We set the links between the roots and targets
 			/*if (highValue == diag){
 				matrix->setRootTarget(matrix->getPosition(i-1,j-1), pos);
@@ -173,11 +251,11 @@ int main(int argc, char** argv){
 				matrix->setRootTarget(matrix->getPosition(i-1,j), pos);
 			}*/
 			//If this value is greater than the previous maximum, we keep the position and the value
-			if (highValue > maxPos->getValue()){
+			/*if (highValue > maxPos->getValue()){
 				maxPos = pos;
-			}
-		}
-	}
+			}*/
+		/*}
+	}*/
 	clock_t end = clock();
 	double time = double(end - begin)/CLOCKS_PER_SEC;
 	int test = 0;
@@ -190,12 +268,13 @@ int main(int argc, char** argv){
 	clock_t end2 = clock();
 	double time2 = double(end2-begin2)/CLOCKS_PER_SEC;
 	cout << "Le test en double for met " << time2 << " secondes" << endl;
-	int score = maxPos->getValue();
+	//int score = maxPos->getValue();
+	int score = maxValue;
 	double lambda = 0.267;
 	double k = 0.041;
 	double bitscore = double(score);
 	bitscore = (lambda*bitscore - log(k))/log(2);
-	cout << "Score donne : "<< bitscore << " en " << time << " secondes"<< endl;
+	cout << "Score donne : "<< score << " en " << time << " secondes"<< endl;
 	//free(allPos);
 	//Pas oublier de delete !!!!
 
