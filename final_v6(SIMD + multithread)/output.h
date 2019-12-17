522d206f7c2f72586076aa5855b8a164735434b5
Kcnarf-exe <francktrouillez99@gmail.com>
2019-12-17 20:54:12 +0100

    Multithreading v2

--- final_v6(SIMD + multithread)/output.h
+++
@@ -1,8 +0,0 @@
-#include "smith.h"
-
-
-
-pair<string, string> readFasta(string file);
-string scoreString(int index, int score, string db, PIN* filePIN, PHR* filePHR, int maxLine);
-string alignementString(vector<int> result ,string query,string db, PIN* filePIN, PHR* filePHR, char dataBase[] , int maxLine);
-void writeOutput(vector<vector<int>> results, string outputFile, string queryFileName, string dataBaseFileName, string queryName, string querySequence, clock_t begin, PIN* filePIN, PSQ* filePSQ);
